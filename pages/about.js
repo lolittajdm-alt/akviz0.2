d5630ec405123aeda3044333979cd5c730b978b5
Влад Чабанюк <vladcabanuk@MacBook-Air-Vlad.local>
2025-08-03 06:06:03 +0300

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <div style={{
+      fontFamily: "Arial, sans-serif",
+      background: "#20232a",
+      color: "white",
+      minHeight: "100vh",
+      padding: "50px"
+    }}>
+      <h1>Про наш проєкт</h1>
+      <p>Akustychno-Visualno — це проєкт, який об’єднує звук і візуал.</p>
+      <a href="/" style={{ color: "lightblue" }}>Назад</a>
+    </div>
+  );
+}
