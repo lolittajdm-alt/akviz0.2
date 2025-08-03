885b78f072e11738db01070bcd23f87e3bb43627
lolittajdm-alt <lolitta.jdm@gmail.com>
2025-08-03 19:43:56 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -219,8 +219,6 @@ export default function Home() {
     form.result ? `ПП: ${form.result}` : null,
     form.description ? `Опис: ${form.description}` : null
   ].filter(Boolean).join("\n");
-
-+
   navigator.clipboard.writeText(txt);
   alert("Скопійовано!");
   return txt;
