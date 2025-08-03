79031d024deba6f2c93ec6dbaaac922d296573e4
lolittajdm-alt <lolitta.jdm@gmail.com>
2025-08-03 19:57:53 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -202,8 +202,8 @@ export default function Home() {
 
   // ——— Копировать/WhatsApp ———
   const copyToClipboard = () => {
-  const txt = (
-  [
+  const txt = [
+  (
     `П: ${form.sector} , ${form.subdivision} , ${form.position}`,
     form.selectedGoals.length ? `Ціль: ${form.selectedGoals.join(", ")}` : null,
     form.side ? `Сторона: ${form.side}` : null,
@@ -219,7 +219,7 @@ export default function Home() {
     form.detectionMethods.length ? `Вияв: ${form.detectionMethods.join(", ")}` : null,
     form.result ? `ПП: ${form.result}` : null,
     form.description ? `Опис: ${form.description}` : null
-  ].filter(Boolean).join("\n")
+  ).filter(Boolean).join("\n"]
 );
 
   navigator.clipboard.writeText(txt);
