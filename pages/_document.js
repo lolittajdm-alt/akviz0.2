188f341961c4448b094416bc0d5a8d6c9b901b4b
lolittajdm-alt <lolitta.jdm@gmail.com>
2025-08-09 07:24:19 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="uk">
       <Head>
         {/* Favicon — положи свой файл favicon.png или favicon.ico в папку /public */}
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
         {/* Если используешь .ico: */}
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
