fb23dbaf99aa32fbda47f98fe5900789db2cb039
lolittajdm-alt <lolitta.jdm@gmail.com>
2025-08-09 07:27:01 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,1 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-
-export default function Document() {
-  return (
-    <Html lang="uk">
-      <Head>
-        {/* Favicon — положи свой файл favicon.png или favicon.ico в папку /public */}
-        <link rel="icon" type="image/png" href="/favicon.ico" />
-        {/* Если используешь .ico: */}
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+<link rel="icon" type="image/png" href="/favicon.png" />
